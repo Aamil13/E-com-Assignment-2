a8c5acfadec18dcb1f68add10f51f8d896433a19
Aamil13 <aamil.shafi13@gmail.com>
2022-09-23 23:10:29 +0530

    bug fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,6 @@ const Home = () => {
         fetch('https://fakestoreapi.com/products?limit=12')
         .then(res=>res.json())
         .then(json=>setData(json))
-        .catch((err)=>alert('Unable to fetch data'));
         setLoading(false)
     },[])
     
@@ -35,18 +34,14 @@ const handlePriceSort = (val)=>{
 const handleFilter = (val)=>{
     let newData = data;
     let priceFiltered = newData.filter((x)=> x.price <= val);
-    setFilteredData([...newData])
+    setFilteredData([...priceFiltered])
 }
   return (
     <>
     <Header handlePriceSort={handlePriceSort} handleFilter={handleFilter}/>
     <div className='container'>
         <h1 className='fw-light text-center'>Latest Listings</h1>
-        {loading &&
-        <div class="spinner-border text-dark text-center my-5" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
-        }
+
         <div className='row justify-content-evenly'>
             {!filteredData.length && data.length ? data.map((item, i)=>(
             <Porduct key={i}  item={item}/>
@@ -54,7 +49,9 @@ const handleFilter = (val)=>{
         : data.length && filteredData.length ? filteredData.map((item, i)=>(
             <Porduct key={i}  item={item}/>
         ))
-        : <h1 className='text-center text-secondary fw-light my-5'>Oops Internal Server Error. Unable to fetch Data!</h1>
+        : !loading &&  <div class="spinner-border text-dark text-center my-5" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
         }
            
         </div>
